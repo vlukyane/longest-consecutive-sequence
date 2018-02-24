882db0fbf59ae12f1ed4e1cfd5fe808f6d8121f4
vlukyaneRubyLabs <vlukyane@gmail.com>
2018-02-24 13:37:53 +0300

    longest-cons-seq

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
-}
+  var s = new Set(), ans = 0;
+  for (var i = 0; i < array.length; i++){
+      s.add(array[i])
+  }
+
+  for (var i = 0; i < array.length; i++){
+      if (s.has(array[i] - 1)){ continue; }
+      var res = 0;
+      item = array[i];
+      while (s.has(item)){
+          res ++;
+          item ++;
+      }
+      ans = Math.max(ans, res);
+  }
+  return ans;
+};
+
+
